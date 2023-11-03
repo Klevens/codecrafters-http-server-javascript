796054b5ef99558b6ff422b4a937a014b874ff7b
Klevens <kelvinreyescnh@gmail.com>
2023-11-03 15:29:35 -0400

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -2,6 +2,14 @@ const net = require("net");
 
 
 const server = net.createServer((socket) => {
+
+    socket.on('data', (dada)=>{
+        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        socket.end();
+        server.close();
+
+    }),
+
   socket.on("close", () => {
     socket.end();
     server.close();
