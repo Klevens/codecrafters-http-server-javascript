9eac64c9dae21482ede5da83acf2cda2c087a71a
Klevens <kelvinreyescnh@gmail.com>
2023-11-05 11:10:30 -0400

    3rd stage

--- app/main.js
+++ app/main.js
@@ -1,12 +1,40 @@
 const net = require("net");
 
+const checkRequest = (req) =>  {
+
+   const stringBuffer = req.toString('utf8').split('\n');
+   const startLine = stringBuffer[0].split(' ');
+
+   return {
+    method : startLine[0],
+    path: startLine[1],
+    version: startLine[2]
+   }
+
+
+}
 
 const server = net.createServer((socket) => {
 
-    socket.on('data', (dada)=>{
-        socket.write('HTTP/1.1 200 OK\r\n\r\n');
-        socket.end();
-        server.close();
+    socket.on('data', (req)=>{
+        const reqInfo = checkRequest(req);
+
+
+        if (reqInfo.path == '/'){
+          socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        
+          socket.end();
+          server.close();
+        } else { 
+
+          socket.write('HTTP/1.1 404 NOT FOUND\r\n\r\n');       
+          socket.end();
+          server.close();
+        }
+
+
+
+
 
     }),
 
