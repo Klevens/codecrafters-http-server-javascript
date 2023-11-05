4b3e0e650d3f0dfc8f191a424d6b052195651f96
Klevens <kelvinreyescnh@gmail.com>
2023-11-05 15:00:27 -0400

    4rd stage

--- app/main.js
+++ app/main.js
@@ -1,47 +1,50 @@
 const net = require("net");
 
-const checkRequest = (req) =>  {
+const checkRequest = (req) => {
+  const stringBuffer = req.toString("utf8").split("\n");
+  const startLine = stringBuffer[0].split(" ");
 
-   const stringBuffer = req.toString('utf8').split('\n');
-   const startLine = stringBuffer[0].split(' ');
+  return {
+    method: startLine[0],
+    path: startLine[1].split("/"),
+    version: startLine[2],
+  };
+};
 
-   return {
-    method : startLine[0],
-    path: startLine[1],
-    version: startLine[2]
-   }
-
-
-}
+const formatBody = (rawBody) => {
+  return (
+    `HTTP/1.1 200 OK\r\n` +
+    `Content-Type: text/plain\r\n` +
+    `Content-Length: ${Buffer.byteLength(rawBody)}\r\n` +
+    `\r\n` +
+    `${rawBody}`
+  );
+};
 
 const server = net.createServer((socket) => {
+  socket.on("data", (req) => {
+    const reqInfo = checkRequest(req);
 
-    socket.on('data', (req)=>{
-        const reqInfo = checkRequest(req);
+    console.log(reqInfo);
 
+    if (reqInfo.path[1] == "") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
 
-        if (reqInfo.path == '/'){
-          socket.write('HTTP/1.1 200 OK\r\n\r\n');
-        
-          socket.end();
-          server.close();
-        } else { 
-
-          socket.write('HTTP/1.1 404 NOT FOUND\r\n\r\n');       
-          socket.end();
-          server.close();
-        }
-
-
-
-
-
-    }),
-
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+      socket.end();
+    } else if (reqInfo.path[1] == "echo") {
+      socket.write(formatBody(reqInfo.path[2]));
+      socket.end();
+    } else {
+      socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+      socket.end();
+    }
+  }),
+    socket.on("close", () => {
+      socket.end();
+      server.close();
+    });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", "", () => {
+  console.log("Port 4221, from localhost");
+});
